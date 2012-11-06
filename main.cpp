050870054d0123aaf6a00185cd90d67c2768c6d3
Danny M <danny.mircea@gmail.com>
2012-11-06 19:28:00 +0200

    this a the second commit for UMCTP

--- main.cpp
+++ main.cpp
@@ -3,4 +3,12 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 	cout<<"Yo nigga"<<endl;
+	if(argc>1) {
+		for(int i=1; i<argc; i++){
+			cout<<argv[i]<<endl;
+		}
+	}
+	else{
+		cout<<""<<endl;
+	}
 }
